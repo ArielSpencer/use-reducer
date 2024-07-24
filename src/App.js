cb4ef9ad03c228364d5cf249c7eb3db4b135c681
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-23 22:28:08 -0300

    Implementar a base do cadastro de frases

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // lista de frases (estado);
+
+  // usuário pode adicionar novas frases, desde que:
+  // a frase possua mais do que 20 caracteres;
+  // a frase seja única;
+
+  const [frase, setFrase] = useState('');
+  const [frases, setFrases] = useState([])
+
+  function salvarFrase(evento) {
+    evento.preventDefault()
+    if (frase.length < 20) {
+      alert("Por Favor, digite uma frase com mais de 20 caracteres.")
+      return
+    }
+    if (frases.includes(frase)) {
+      alert("Não são permitidas frases duplicadas.")
+      return
+    }
+    setFrases([...frases, frase])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <p>Bem-vindo ao mundo do useReducer.</p>
-      </header>
+      <form onSubmit={salvarFrase}>
+        <textarea
+          required
+          value={frase}
+          onChange={evento => setFrase(evento.target.value)}
+          placeholder="Digite sua frase..."
+        />
+        <br />
+        <button>Salvar frase</button>
+      </form>
+      {frases.map((fraseAtual, index) => <p key={index}>{fraseAtual}</p>)}
     </div>
   );
 }
